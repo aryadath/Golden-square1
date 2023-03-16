d0a78f3b46c26f63cbafe094735a9ee9d02fd945
Arya Sunildath <aryamakers@MASTUDENT-Arya-Sunildath-C17GM6AXQ6L4.local>
2023-03-16 11:15:25 +0000

    Used an () not a {} for the true statement because we are not testing for an error here

--- spec/password_checker_spec.rb
+++ spec/password_checker_spec.rb
@@ -1,11 +1,13 @@
 require 'password_checker'
 
 RSpec.describe PasswordChecker do
-  context 'throws errors correctly' do
-    it 'password is not greater than or equal to 8' do
-      password_checker = PasswordChecker.new
-      password_checker.check('mountain')
-      expect {password_checker.check('cow') }.to raise_error "Invalid password, must be 8+ characters."
-    end
+  it 'returns true if password is more than 8' do
+    password_checker = PasswordChecker.new
+    expect(password_checker.check('makersacademy')).to eq true
+  end
+
+  it 'password is less than 8' do
+    password_checker = PasswordChecker.new
+    expect {password_checker.check('cow') }.to raise_error "Invalid password, must be 8+ characters."
   end
 end
